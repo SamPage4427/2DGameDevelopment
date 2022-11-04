ac09a8e6ed8c168222dbf845a70a052ee645e456
SamPage4427 <sampage4427@gmail.com>
2022-11-04 17:12:05 -0600

    Basic 2D Java Treasure Game

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # First 2D Treasure Adventure game created with Java.
-Learned through a tutorial on YouTube to create a basic 2D java-based 16-bit Treasure adventure game.
+
+A full recreation of a 2D game tutorial on YouTube. Characters, scenery and Code do not belong to me they belong to @RyiSnow.
+I used this as a learning tool to develop my Java skills and build my ability to create my own version.
 
 ## Credit RyiSnow on YouTube
---
+++ src/main/AssetSet.java
@@ -0,0 +1,60 @@
+package main;
+
+import object.Chest;
+import object.Door;
+import object.Key;
+import object.Boots;
+import object.SuperObject;
+
+public class AssetSet {
+    GamePanel gp;
+
+    public AssetSet(GamePanel gp) {
+        this.gp = gp;
+    }
+    public void setObject() {
+        gp.obj[0] = new Key();
+        gp.obj[0].worldX = 23 * gp.tileSize;
+        gp.obj[0].worldY = 10 * gp.tileSize;
+
+        gp.obj[1] = new Key();
+        gp.obj[1].worldX = 5 * gp.tileSize;
+        gp.obj[1].worldY = 26 * gp.tileSize;
+
+        gp.obj[2] = new Key();
+        gp.obj[2].worldX = 25 * gp.tileSize;
+        gp.obj[2].worldY = 42 * gp.tileSize;
+
+        gp.obj[3] = new Door();
+        gp.obj[3].worldX = 43 * gp.tileSize;
+        gp.obj[3].worldY = 33 * gp.tileSize;
+
+        gp.obj[4] = new Door();
+        gp.obj[4].worldX = 6 * gp.tileSize;
+        gp.obj[4].worldY = 11 * gp.tileSize;
+
+        gp.obj[5] = new Door();
+        gp.obj[5].worldX = 3 * gp.tileSize;
+        gp.obj[5].worldY = 45 * gp.tileSize;
+
+        gp.obj[6] = new Boots();
+        gp.obj[6].worldX = 43 * gp.tileSize;
+        gp.obj[6].worldY = 28 * gp.tileSize;
+
+        gp.obj[7] = new Chest();
+        gp.obj[7].worldX = 3 * gp.tileSize;
+        gp.obj[7].worldY = 3 * gp.tileSize;
+
+        gp.obj[8] = new Chest();
+        gp.obj[8].worldX = 11 * gp.tileSize;
+        gp.obj[8].worldY = 3 *gp.tileSize;
+
+        gp.obj[9] = new Chest();
+        gp.obj[9].worldX = 3 * gp.tileSize;
+        gp.obj[9].worldY = 42 * gp.tileSize;
+
+        gp.obj[10] = new Boots();
+        gp.obj[10].worldX = 21 * gp.tileSize;
+        gp.obj[10].worldY = 5 * gp.tileSize;
+    }
+}
